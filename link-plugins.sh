4de03e2590d95b77c20608962a3bd4b714dc8a41
Gerard van Helden <gerard@zicht.nl>
2015-06-16 21:35:06 +0000

    made link-plugins work with repositories exposing multiple plugins
    git-svn-id: svn://zon/projecten/zicht-libraries/z-installer/trunk@63980 0f35748b-8228-47f0-bbea-889afbbd9a42

--- link-plugins.sh
+++ link-plugins.sh
@@ -1,6 +1,8 @@
 for d in vendor/zicht/z-plugin-*; do
     dir=$(cd $d && pwd)
-    ( cd plugins && ln -s $dir/*/ );
+    for sd in $dir/*/; do
+        ( cd plugins && ln -s $sd -v );
+    done;
 done;
 
 symlinks -cr ./plugins
