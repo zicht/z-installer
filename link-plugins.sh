d6d50559ae0cc4058478ba77be2b11a8c1a6e2d0
Victor Moreno Alhambra <victor@zicht.nl>
2015-09-17 18:49:47 +0000

    minor mac compat issue fixed
    git-svn-id: svn://zon/projecten/zicht-libraries/z-installer/trunk@69183 0f35748b-8228-47f0-bbea-889afbbd9a42

--- link-plugins.sh
+++ link-plugins.sh
@@ -1,7 +1,7 @@
 for d in vendor/zicht/z-plugin-*; do
     dir=$(cd $d && pwd)
     for sd in $dir/*/; do
-        ( cd plugins && ln -s $sd -v );
+        ( cd plugins && ln -s -v $sd );
     done;
 done;
 
